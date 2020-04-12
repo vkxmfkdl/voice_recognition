bcd3eaa860a80c0e87c5a45d95ad3d065b478bb6
vkxmfkdl <vkxmfkdl@gmail.com>
2020-04-12 13:07:09 +0900

    2020-04-12 수정

--- uploadproject/uploadapp/templatetags/tag_library.py
+++ uploadproject/uploadapp/templatetags/tag_library.py
@@ -2,6 +2,10 @@ from django import template
 
 register = template.Library()
 
-@register.filter()
+@register.filter(name='int')
 def to_int(value):
     return int(value)
+
+@register.filter(name='str')
+def to_string(value):
+    return str(value)
--- voiceweb/conference/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConferenceConfig(AppConfig):
-    name = 'conference'
